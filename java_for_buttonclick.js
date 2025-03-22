865ac5d7f385bc7363a2f26f91395fd07fbce754
JM Smaron <jmsmaron2003@gmail.com>
2025-03-22 23:12:17 +0600

    write the function for cart

--- java_for_buttonclick.js
+++ java_for_buttonclick.js
@@ -5,3 +5,11 @@ function showAlert() {
 function submitForm() {
     alert("Thank you for reaching out! We'll get back to you soon.");
 }
+
+
+let totalAmount = 0;
+
+function addToCart(price) {
+    totalAmount += price;
+    document.getElementById("totalAmount").innerText = totalAmount;
+}
