9c16c794694603d85c8aaac074e7eb75eb7fa630
JM Smaron <jmsmaron2003@gmail.com>
2025-03-22 23:17:21 +0600

    button for adding or deleting product

--- java_for_buttonclick.js
+++ java_for_buttonclick.js
@@ -8,8 +8,24 @@ function submitForm() {
 
 
 let totalAmount = 0;
+let cartItems = { pasta: 0, burger: 0, chicken: 0, salad: 0 };
 
-function addToCart(price) {
-    totalAmount += price;
+function updateCart(item, price) {
+    if (price > 0) {
+        cartItems[item]++;
+    } else {
+        if (cartItems[item] > 0) {
+            cartItems[item]--;
+        }
+    }
+
+    // Update the total amount
+    totalAmount = (cartItems["pasta"] * 250) + 
+                  (cartItems["burger"] * 300) + 
+                  (cartItems["chicken"] * 450) + 
+                  (cartItems["salad"] * 400);
+
+    // Update UI
+    document.getElementById(item + "-count").innerText = cartItems[item];
     document.getElementById("totalAmount").innerText = totalAmount;
 }
